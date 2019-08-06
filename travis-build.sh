aeb6f9f0a016e4aafe00a35fb0376f391181bf23
Pierre Duchemin <pierre.duchemin@savoirfairelinux.com>
2019-08-06 15:53:24 -0400

    Fix Update travis script

--- travis-build.sh
+++ travis-build.sh
@@ -2,7 +2,7 @@
 set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    ./gradlew clean gandalf:gnagCheck example:assembleDebug
+    ./gradlew clean example:assembleDebug
 else
-    ./gradlew clean gandalf:gnagReport example:assembleDebug -PauthToken="${GNAG_AUTH_TOKEN}" -PissueNumber="${TRAVIS_PULL_REQUEST}"
+    ./gradlew clean example:assembleDebug -PissueNumber="${TRAVIS_PULL_REQUEST}"
 fi

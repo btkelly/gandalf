e1a3ebb078a7f1d521dbee638c168af54a4f1dd2
Pierre Duchemin <pierre.duchemin@savoirfairelinux.com>
2019-08-28 10:35:52 -0400

    Fix put gnag plugin back in project

--- travis-build.sh
+++ travis-build.sh
@@ -2,7 +2,7 @@
 set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    ./gradlew clean example:assembleDebug
+    ./gradlew clean gandalf:gnagCheck example:assembleDebug
 else
-    ./gradlew clean example:assembleDebug -PissueNumber="${TRAVIS_PULL_REQUEST}"
+    ./gradlew clean gandalf:gnagReport example:assembleDebug -PauthToken="${GNAG_AUTH_TOKEN}" -PissueNumber="${TRAVIS_PULL_REQUEST}"
 fi
